5a1e96a6a2d494f1f6931e42dae151444c7a0b15
cogni8r <57598620+cogni8r@users.noreply.github.com>
2022-12-14 08:32:26 +0500

    feat: update rules

--- eslint-rules/typescript.js
+++ eslint-rules/typescript.js
@@ -5,23 +5,23 @@ module.exports = {
   rules: {
     // Supported Rules - https://github.com/typescript-eslint/typescript-eslint/tree/master/packages/eslint-plugin#supported-rules.
 
-    '@typescript-eslint/array-type': 'off', // It's useful to use different styles of array for different situations.
-    '@typescript-eslint/ban-ts-comment': [
-      'error',
+    "@typescript-eslint/array-type": "off", // It's useful to use different styles of array for different situations.
+    "@typescript-eslint/ban-ts-comment": [
+      "error",
       {
-        'ts-expect-error': 'allow-with-description',
-        'ts-ignore': 'allow-with-description',
-        'ts-nocheck': 'allow-with-description',
-        'ts-check': 'allow-with-description',
+        "ts-expect-error": "allow-with-description",
+        "ts-ignore": "allow-with-description",
+        "ts-nocheck": "allow-with-description",
+        "ts-check": "allow-with-description",
       },
     ],
     // Useless - only for public fields of classes with literal type.
     // very narrow and complex rule.
-    '@typescript-eslint/class-literal-property-style': 'off',
-    '@typescript-eslint/consistent-type-definitions': ['error', 'interface'],
+    "@typescript-eslint/class-literal-property-style": "off",
+    "@typescript-eslint/consistent-type-definitions": ["error", "interface"],
 
-    '@typescript-eslint/explicit-function-return-type': [
-      'error',
+    "@typescript-eslint/explicit-function-return-type": [
+      "error",
       {
         allowExpressions: true,
         allowTypedFunctionExpressions: true,
@@ -30,78 +30,83 @@ module.exports = {
         allowConciseArrowFunctionExpressionsStartingWithVoid: true,
       },
     ],
-    '@typescript-eslint/explicit-member-accessibility': ['error', { accessibility: 'no-public' }],
-    '@typescript-eslint/member-delimiter-style': 'off', // Prettier.
-    '@typescript-eslint/member-ordering': 'off', // It's better to order elements by severity (not by member type).
-    '@typescript-eslint/method-signature-style': 'off', // Useless.
-    '@typescript-eslint/naming-convention': [
-      'error',
+    "@typescript-eslint/explicit-member-accessibility": [
+      "error",
+      { accessibility: "no-public" },
+    ],
+    "@typescript-eslint/member-delimiter-style": "off", // Prettier.
+    "@typescript-eslint/method-signature-style": "off", // Useless.
+    "@typescript-eslint/naming-convention": [
+      "error",
       {
-        selector: 'default',
-        format: ['camelCase'],
-        leadingUnderscore: 'allow',
-        trailingUnderscore: 'allow',
+        selector: "default",
+        format: ["camelCase"],
+        leadingUnderscore: "allow",
+        trailingUnderscore: "allow",
       },
 
       {
-        selector: 'variable',
-        format: ['camelCase', 'UPPER_CASE'],
-        leadingUnderscore: 'forbid',
-        trailingUnderscore: 'forbid',
+        selector: "variable",
+        format: ["camelCase", "UPPER_CASE"],
+        leadingUnderscore: "forbid",
+        trailingUnderscore: "forbid",
       },
 
       {
-        selector: 'typeLike',
-        format: ['PascalCase'],
+        selector: "typeLike",
+        format: ["PascalCase"],
       },
       {
-        selector: 'interface',
-        format: ['PascalCase'],
+        selector: "interface",
+        format: ["PascalCase"],
         custom: {
-          regex: '^I[A-Z]',
+          regex: "^I[A-Z]",
           match: false,
         },
       },
       {
-        selector: 'memberLike',
-        modifiers: ['private'],
-        format: ['camelCase'],
-        leadingUnderscore: 'require',
+        selector: "memberLike",
+        modifiers: ["private"],
+        format: ["camelCase"],
+        leadingUnderscore: "require",
       },
     ],
-    '@typescript-eslint/no-empty-interface': 'off', // Sometimes we need them to mark a class with an interface.
+    "@typescript-eslint/no-empty-interface": "off", // Sometimes we need them to mark a class with an interface.
     // These classes can be useful with concept of DI and code generation.
-    '@typescript-eslint/neous-class': 'off',
-    '@typescript-eslint/no-inferrable-types': 'off', // It's better to declare variables with concrete types.
-    '@typescript-eslint/no-parameter-properties': 'off', // It's very convenient for DI.
-    '@typescript-eslint/no-type-alias': 'off', // It's better to be able to use different approaches to describe types.
-    '@typescript-eslint/no-unnecessary-condition': [
-      'error',
+    "@typescript-eslint/neous-class": "off",
+    "@typescript-eslint/no-inferrable-types": "off", // It's better to declare variables with concrete types.
+    "@typescript-eslint/no-type-alias": "off", // It's better to be able to use different approaches to describe types.
+    "@typescript-eslint/no-unnecessary-condition": [
+      "error",
       {
         allowConstantLoopConditions: true,
       },
     ], // Default options have been changed.
-    '@typescript-eslint/no-unnecessary-qualifier': 'off', // Conflicts with 'no-namespace' rule.
+    "@typescript-eslint/no-unnecessary-qualifier": "off", // Conflicts with 'no-namespace' rule.
     // sometimes it's better to specify type argument explicitly.
-    '@typescript-eslint/no-unnecessary-type-arguments': 'off',
-    '@typescript-eslint/no-unused-vars-experimental': 'off', // Implemented in typescript.
-    '@typescript-eslint/no-var-requires': 'off', // Conflicts with '@typescript-eslint/no-require-imports' rule.
-    '@typescript-eslint/prefer-for-of': 'off', // Useless.
-    '@typescript-eslint/prefer-function-type': 'off', // It's better to be able to use different approaches to describe types.
-    '@typescript-eslint/prefer-includes': 'off', // Useless.
-    '@typescript-eslint/prefer-namespace-keyword': 'off', // Conflicts with 'no-namespace' rule.
+    "@typescript-eslint/no-unnecessary-type-arguments": "off",
+    "@typescript-eslint/no-unused-vars-experimental": "off", // Implemented in typescript.
+    "@typescript-eslint/no-var-requires": "off", // Conflicts with '@typescript-eslint/no-require-imports' rule.
+    "@typescript-eslint/parameter-properties": [
+      true,
+      {
+        prefer: "parameter-property",
+      },
+    ],
+    "@typescript-eslint/prefer-includes": "off", // Useless.
+    "@typescript-eslint/prefer-namespace-keyword": "off", // Conflicts with 'no-namespace' rule.
     // it's not very convenient for DI and sometimes mutations are good.
-    '@typescript-eslint/prefer-readonly-parameter-types': 'off',
-    '@typescript-eslint/prefer-string-starts-ends-with': 'off', // Useless.
-    '@typescript-eslint/promise-function-async': 'off', // Useless if we don't await some result.
-    '@typescript-eslint/restrict-template-expressions': [
-      'error',
+    "@typescript-eslint/prefer-readonly-parameter-types": "off",
+    "@typescript-eslint/prefer-string-starts-ends-with": "off", // Useless.
+    "@typescript-eslint/promise-function-async": "off", // Useless if we don't await some result.
+    "@typescript-eslint/restrict-template-expressions": [
+      "error",
       { allowNumber: true, allowBoolean: true },
     ],
     // Sometimes it's better to use sort types manually. Some of them are more important than others.
-    '@typescript-eslint/sort-type-union-intersection-members': 'off',
-    '@typescript-eslint/typedef': [
-      'error',
+    "@typescript-eslint/sort-type-union-intersection-members": "off",
+    "@typescript-eslint/typedef": [
+      "error",
       {
         arrayDestructuring: false,
         arrowParameter: false,
@@ -120,30 +125,34 @@ module.exports = {
 
     // Extension Rules - https://github.com/typescript-eslint/typescript-eslint/tree/master/packages/eslint-plugin.
 
-    '@typescript-eslint/init-declarations': 'off', // Typescript checks unassigned variables using.
-    '@typescript-eslint/lines-between-class-members': [
-      'error',
-      'always',
+    "@typescript-eslint/init-declarations": "off", // Typescript checks unassigned variables using.
+    "@typescript-eslint/lines-between-class-members": [
+      "error",
+      "always",
       { exceptAfterSingleLine: true },
     ], // Default options have been changed
-    '@typescript-eslint/no-magic-numbers': [
-      'error',
+    "@typescript-eslint/no-magic-numbers": [
+      "error",
       {
         // eslint-disable-next-line no-magic-numbers
         ignore: [-1, 0, 1],
         ignoreNumericLiteralTypes: true,
         ignoreReadonlyClassProperties: true,
         ignoreEnums: true,
+        ignoreTypeIndexes: true,
       },
     ],
-     "@typescript-eslint/no-restricted-imports": "off", // Nothing to restrict
-    '@typescript-eslint/no-unused-expressions': 'off', // Typescript checks this.
-    '@typescript-eslint/no-unused-vars': 'off', // Implemented in typescript.
+    "@typescript-eslint/no-restricted-imports": "off", // Nothing to restrict
+    "@typescript-eslint/no-unused-expressions": "off", // Typescript checks this.
+    "@typescript-eslint/no-unused-vars": "off", // Implemented in typescript.
     // it's better to have ability to organize code inside file by priority and not by define order.
-    '@typescript-eslint/no-use-before-define': 'off',
-    '@typescript-eslint/return-await': 'off', // Useless.
+    "@typescript-eslint/no-use-before-define": "off",
+    "@typescript-eslint/return-await": "off", // Useless.
 
-    '@typescript-eslint/restrict-plus-operands': ['error', { checkCompoundAssignments: true }],
-    '@typescript-eslint/unbound-method': 'off', // We can use bind here.
+    "@typescript-eslint/restrict-plus-operands": [
+      "error",
+      { checkCompoundAssignments: true },
+    ],
+    "@typescript-eslint/unbound-method": "off", // We can use bind here.
   },
 };
